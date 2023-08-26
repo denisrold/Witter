f1eaec960db166f58707ea3f4729b12520a12c21
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-26 13:39:52 -0300

    Google Botton

--- t-witter-clone/pages/index.js
+++ t-witter-clone/pages/index.js
@@ -1,6 +1,3 @@
-
 export default function Home() {
-  return (
-   <div>Home</div>
-  )
+  return <div className="flex items-center justify-center h-screen">Home</div>;
 }
--- t-witter-clone/pages/login.js
+++ t-witter-clone/pages/login.js
@@ -1,3 +1,27 @@
-export default function LoginPage() {
-  return <div>Login</div>;
+import { getProviders, signIn } from "next-auth/react";
+export default function LoginPage({ providers }) {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      {Object.values(providers).map((provider) => (
+        <div>
+          <button
+            onClick={async () => {
+              await signIn(provider.id);
+            }}
+            className="bg-twitterWhite py-2 text-black rounded-full flex items-center pl-3.5 pr-4"
+          >
+            <img src="/google.png" alt="" className="h-7 mr-1"></img>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
 }
