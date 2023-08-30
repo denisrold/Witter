51efee4c5fb4a96dcbcea0c35b17657e6a33d651
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-30 12:57:17 -0300

    PostButtons

--- t-witter-clone/components/PostButtons.js
+++ t-witter-clone/components/PostButtons.js
@@ -1,14 +1,14 @@
 export default function PostButtons() {
   return (
-    <div className="flex justify-between">
-      <div className="flex">
+    <div className="flex justify-between mr-12 text-twitterLightGray text-sm mt-1">
+      <button className="flex">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-6 h-6"
+          className="w-5 h-5 mr-1"
         >
           <path
             strokeLinecap="round"
@@ -17,15 +17,15 @@ export default function PostButtons() {
           />
         </svg>
         <span>0</span>
-      </div>
-      <div className="flex">
+      </button>
+      <button className="flex">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-6 h-6"
+          className="w-5 h-5 mr-1"
         >
           <path
             strokeLinecap="round"
@@ -34,15 +34,15 @@ export default function PostButtons() {
           />
         </svg>
         <span>0</span>
-      </div>
-      <div className="flex">
+      </button>
+      <button className="flex">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           stroke-width="1.5"
           stroke="currentColor"
-          class="w-6 h-6"
+          class="w-5 h-5 mr-1"
         >
           <path
             stroke-linecap="round"
@@ -51,15 +51,15 @@ export default function PostButtons() {
           />
         </svg>
         <span>0</span>
-      </div>
-      <div className="flex">
+      </button>
+      <button className="flex">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           stroke-width="1.5"
           stroke="currentColor"
-          class="w-6 h-6"
+          class="w-5 h-5 mr-1"
         >
           <path
             stroke-linecap="round"
@@ -67,7 +67,7 @@ export default function PostButtons() {
             d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
           />
         </svg>
-      </div>
+      </button>
     </div>
   );
 }
