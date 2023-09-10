cc7fb115024ced391497e4131f68e01dbad2d39b
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-10 19:22:31 -0300

    favIcon witter

--- t-witter-clone/pages/_document.js
+++ t-witter-clone/pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>The Witter</title>
+      <link rel="shortcut icon" href="/witter.jpg" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
